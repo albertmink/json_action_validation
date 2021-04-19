f71cd2bfecd9c07c83a69055269256df3f7a86f5
Albert Mink <albert.mink@sap.com>
2021-04-19 15:24:33 +0200

    trigger

--- script.sh
+++ script.sh
@@ -3,3 +3,4 @@ ajv compile --strict=false -s clas/clas.json
 ajv compile -s clas/clas.json
 ajv validate --spec=draft2020 --strict=false -s clas/clas.json -d clas/examples/zcl_aff_example.clas.json
 ajv validate --spec=draft2020 --strict=false -s clas/clas.json -d clas/examples/zcl_aff_example.bad.clas.json
+
