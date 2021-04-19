73c3a0203f3efc09dc0d770da3df06f9c3de6877
Albert Mink <albert.mink@sap.com>
2021-04-19 14:38:53 +0200

    Revert "raw err msg"
    This reverts commit f97eb7c8fad2738b7a789650013ab6ca64a8ae9f.

--- json-validator.py
+++ json-validator.py
@@ -73,7 +73,14 @@ def validate_json( schema, examples):
                 print(f"::error file={example} ::{ex.msg}")
                 nb_errors += 1
             else:
-                validate( json_clas, schema_clas )
+                try:
+                    validate( json_clas, schema_clas )
+                except jsonschema.exceptions.ValidationError as exVal:
+                    nb_errors += 1
+                    print(f"::error file={example},line=1,col=1::{os.path.basename(example).ljust(31)} valid instance {exVal.message}")
+                else:
+                    #print(f"::set-output name={os.path.basename(example).ljust(31)} valid instance of schema {os.path.basename(schema)}" )
+                    print(os.path.basename(example).ljust(31) + " valid instance of schema " + os.path.basename(schema))
 
 
 def validate_json_and_example( json_schemata, repo_obj ):
